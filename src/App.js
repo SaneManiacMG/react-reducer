ba8ef32fd33893df074d717ee5bd0e1a778ad21e
SaneManiacMG <mmoroane@hotmail.com>
2023-11-07 15:19:30 +0200

    Using useReducer with context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import "./App.css";
-import CounterTwo from "./components/CounterTwo";
+import React, { useReducer } from "react";
+import DataFetchingOne from "./components/DataFetchingOne";
+
+export const CountContext = React.createContext();
+
+const initialState = 0;
+const reducer = (state, action) => {
+	switch (action) {
+		case "increment":
+			return state + 1;
+		case "decrement":
+			return state - 1;
+		case "reset":
+			return initialState;
+		default:
+			return state;
+	}
+};
 
 function App() {
+	const [count, dispatch] = useReducer(reducer, initialState);
+
 	return (
-		<div className="App">
-			<h1>React App</h1>
-			<CounterTwo />
-		</div>
+		<CountContext.Provider
+			value={{ countState: count, countDispatch: dispatch }}
+		>
+			<div className="App">
+				<DataFetchingOne />
+			</div>
+		</CountContext.Provider>
 	);
 }
 
