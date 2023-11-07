bbb2e397638796ee29f016daf2a9be27e3967039
SaneManiacMG <mmoroane@hotmail.com>
2023-11-07 14:03:53 +0200

    useReducer with Object

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import CounterOne from "./components/CounterOne";
+import CounterTwo from "./components/CounterTwo";
 
 function App() {
 	return (
 		<div className="App">
 			<h1>React App</h1>
-			<CounterOne />
+			<CounterTwo />
 		</div>
 	);
 }
